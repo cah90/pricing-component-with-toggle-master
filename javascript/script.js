5b41cac3b16e04c4f481597bcb79e81d3eeb3d03
Cássia <cascrisbern@gmail.com>
2021-07-16 15:45:17 -0300

    The follow changes were made: It was added both images It was used the position property to set the images in the right place It was improved the JS code in order to change the prices

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,17 @@
-const cardPrice = document.querySelectorAll(".card__price")
+const cardPrice = document.querySelectorAll(".price__value")
 const checkedValue = document.querySelector('#checkbox')
 
 
 function changePrice() {
-  checkedValue.removeAttribute("checked")
-
-  checkedValue.setAttribute("unchecked", "unchecked")
-  
   console.log(checkedValue.checked)
 
   if(checkedValue.checked == false) {
-    cardPrice[1].innerHTML = '<span class="price__symbol">&dollar;</span> 249.99'
-    cardPrice[0].innerHTML = '<span class="price__symbol">&dollar;</span> 199.99'
-    cardPrice[2].innerHTML = '<span class="price__symbol">&dollar;</span> 399.99'
+    cardPrice[1].innerHTML = '249.99'
+    cardPrice[0].innerHTML = '199.99'
+    cardPrice[2].innerHTML = '399.99'
   } else {
-    cardPrice[1].innerHTML = '<span class="price__symbol">&dollar;</span> 19.99'
-    cardPrice[0].innerHTML = '<span class="price__symbol">&dollar;</span> 24.99'
-    cardPrice[2].innerHTML = '<span class="price__symbol">&dollar;</span> 39.99'
+    cardPrice[1].innerHTML = '19.99'
+    cardPrice[0].innerHTML = '24.99'
+    cardPrice[2].innerHTML = '39.99'
   }
 }
